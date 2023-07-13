dd0a68296986b249f90544b7c417a5bd81f0d747
Büşra Kocakuşaklı <busrakocakusakli@gmail.com>
2023-07-13 14:36:55 +0300

    feat: implement view components

--- RotaAnkaraApp/Controllers/OnboardingViewController.swift
+++ RotaAnkaraApp/Controllers/OnboardingViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnboardingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red    }
+
+    }
 
 
 }
