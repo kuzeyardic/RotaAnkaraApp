d84a5133fce5c891ee790585d1aab50da3975c08
Büşra Kocakuşaklı <busrakocakusakli@gmail.com>
2023-07-13 17:49:03 +0300

    feat: implement button style

---
+++ Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  RotaAnkaraApp
+//
+//  Created by Büşra Kocakuşaklı on 13.07.2023.
+//
+
+import Foundation
+import UIKit
+struct  Constants{
+    
+    
+    
+    struct Colors{
+        static var: UIColor
+    }
+}
--- RotaAnkaraApp/Controllers/OnboardingViewController.swift
+++ RotaAnkaraApp/Controllers/OnboardingViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Register Button
+        registerButton.backgroundColor = .red
+        registerButton.layer.cornerRadius = 33
+        registerButton.setTitle("Hesap Oluştur", for: .normal)
+        registerButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        registerButton.contentEdgeInsets = UIEdgeInsets(top: 13, left: 55, bottom: 13, right: 55)
+       
+        
+        // Login Button
+        loginButton.backgroundColor = .red
+        loginButton.layer.cornerRadius = 33
+        loginButton.setTitle("Giriş Yap", for: .normal)
+        loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        loginButton.contentEdgeInsets = UIEdgeInsets(top: 13, left: 55, bottom: 13, right: 55)
+       
     }
+}
 
-
+extension UIView {
+    func applyGradient(colors: [UIColor]) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors.map { $0.cgColor }
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
 
