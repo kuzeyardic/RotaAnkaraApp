37b8dbda5a341dc166803fe40cba61cfe0cc3db3
Berke Kuzey Ardıç <kuzey.ardic@ug.bilkent.edu.tr>
2023-07-13 11:32:41 +0300

    Initial Commit

---
+++ RotaAnkaraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RotaAnkaraApp
+//
+//  Created by Berke Kuzey Ardıç on 13.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
