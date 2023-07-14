86af0f7c2ed0cdb542ae6c739e600dac50b797f8
Berke Kuzey Ardıç <kuzey.ardic@ug.bilkent.edu.tr>
2023-07-14 17:09:11 +0300

    feat: forgotPassword uı implemented

--- RotaAnkaraApp/Controllers/ForgotPasswordViewController.swift
+++ RotaAnkaraApp/Controllers/ForgotPasswordViewController.swift
@@ -9,18 +9,24 @@ import UIKit
 
 class ForgotPasswordViewController: UIViewController {
 
+    @IBOutlet weak var emailFieldForgotPassword: UITextField!
+    @IBOutlet weak var bbb1: UIButton!
     @IBOutlet weak var sendToRefreshButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
         view.backgroundColor = .black
+        bbb1.setButton(title: "")
         sendToRefreshButton.setButton(title: "Gönder")
         cancelButton.setButton(title: "İptal Et")
         
     }
     
 
-   
+    @IBAction func emailFieldForgotPasswordPressed(_ sender: UITextField) {
+        emailFieldForgotPassword.text = ""
+    }
+    
 
 }
